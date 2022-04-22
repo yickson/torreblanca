93b0eb6be20444583d6638885286cd16098cddb5
Yickson Ramirez <yicksonr@gmail.com>
2022-04-21 23:57:54 -0400

    Favicon y algunos detalles

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,13 @@
 module.exports = {
     title: 'Torre Blanca',
     description: 'Historia de un tiempo sin fin',
+    head: [
+      ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/apple-touch-icon.png"}],
+      ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicon-32x32.png"}],
+      ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicon-16x16.png"}],
+      ['link', { rel: "manifest", href: "/assets/site.webmanifest"}],
+      ['link', { rel: "shortcut icon", href: "/assets/favicon.ico"}],
+    ],
     themeConfig: {
       nav: [
         { text: 'Inicio', link: '/' },
